05dcc40d9d06f9b975e7090f6c3e16bbc3b71505
Edivan da Silva Junior <edivanjr13@gmail.com>
2023-07-10 16:58:59 -0300

    Exercício001

---
+++ curso-onebitcode/logica-de-programacao/ex001/calculadora/script.js
@@ -0,0 +1,15 @@
+const x = parseFloat(prompt('Informe o primeiro número:'))
+const y = parseFloat(prompt('Informe o segundo número:'))
+
+const soma = x + y
+const subtracao = x -y
+const multiplicacao = x * y
+const divisao = x / y
+
+alert(
+    'Resultado:\n' +
+    '\nSoma: ' + soma +
+    '\nSubtração ' + subtracao +
+    '\nMultiplicação ' + multiplicacao +
+    '\nDivisão ' + divisao
+)
