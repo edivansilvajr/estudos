d4db1a93b8b958e18d21fde6b7ea66d13d93548d
Edivan da Silva Junior <edivanjr13@gmail.com>
2024-05-24 17:56:03 -0300

    Aula014 streams e buffers

---
+++ curso-onebitcode/introducao-nodejs/aula014-streams-buffers/index.js
@@ -0,0 +1,19 @@
+const fs = require("node:fs");
+
+//const streamLeitura = fs.createReadStream("./arquivo.txt");
+const streamLeitura = fs.createReadStream("./wallpaper.png");
+const buffer = [];
+
+streamLeitura.on("data", (chunk) => {
+    buffer.push(chunk);
+    console.log("Um chunk foi processado.");
+});
+
+streamLeitura.on("end", () => {
+    console.log("Buffer:\n", buffer);
+    const dadosCompletos = Buffer.concat(buffer).toString();
+    console.log("Dados lidos\n", dadosCompletos);
+});
+
+
+
