f4b320f68b544cf7dc5791307e31534445b43958
Edivan da Silva Junior <edivanjr13@gmail.com>
2024-05-28 09:52:54 -0300

    Aula015 parte2

--- curso-onebitcode/introducao-nodejs/aula015-intercoes-no-terminal/index.js
+++ curso-onebitcode/introducao-nodejs/aula015-intercoes-no-terminal/index.js
@@ -12,7 +12,23 @@ const rl = readline.createInterface({ input: process.stdin, output: process.stdo
   rl.write(`Você digitou: "${input}"`)
 }) */
 
-rl.question("Qual é o seu nome?\n", (answer) => {
+/* rl.question("Qual é o seu nome?\n", (answer) => {
   rl.write(`Olá, ${answer}! \n `)
   rl.close()
-})                                                                      
+}) */
+
+rl.once("close", () => {
+  rl.write("Saindo...");
+  process.exit(0);
+});
+
+rl.on("SIGINT", () => {
+  rl.question("Tem certeza que deseja encerrar a aplicação?", (answer) => {
+    if (answer.trim().toLowerCase() === "s") {
+      rl.write("Saindo...");
+      process.exit(0);
+    } else {
+      rl.write("Você escolheu continuar.");
+    }
+  });
+});
