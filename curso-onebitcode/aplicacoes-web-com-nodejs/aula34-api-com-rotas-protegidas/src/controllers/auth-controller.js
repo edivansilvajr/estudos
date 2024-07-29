d595ca53b3f7badd2cf5cf43fd78aec4d5090c29
Edivan da Silva Junior <edivanjr13@gmail.com>
2024-07-29 17:48:59 -0300

    Aula 35 parte01 API com rotas protegidas

---
+++ curso-onebitcode/aplicacoes-web-com-nodejs/aula34-api-com-rotas-protegidas/src/controllers/auth-controller.js
@@ -0,0 +1,42 @@
+const users = require("../models/users");
+const jwt = require("jsonwebtoken");
+
+const secretKey = "chave-secreta-jwt";
+
+module.exports = {
+    // POST /auth/register
+    register: (req, res) => {
+        const { name, email, password } = req.body;
+
+        if (typeof name !== "string" || typeof email !== "string" || typeof password !== "string") {
+            return res.status(401).json({ message: "Invalid fields!" });
+        }
+
+        const registeredUser = users.registerUser(name, email, password);
+
+        if (!registeredUser) {
+            return res.status(400).json({ message: "Email already in use" });
+        }
+
+        res.status(201).json(registeredUser);
+    },
+
+    // POST /auth/login
+    login: (req, res) => {
+        const { email, password } = req.body;
+
+        if (typeof email !== "string" || typeof password !== "string") {
+            return res.status(401).json({ message: "Invalid fields!" });
+        }
+
+        const user = users.findByEmail(email);
+
+        if (!user) return res.status(404).json({ message: "User not found!" });
+        if (user.password !== password) return res.status(400).json({ message: "Invalid credentials" });
+
+        const payload = { id: user.id, email: user.email };
+        const token = jwt.sign(payload, secretKey, { expiresIn: "1d" });
+
+        res.json({ token });
+    }
+};
